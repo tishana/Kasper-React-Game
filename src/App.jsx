3f9be747f1cde0e61987896638e6f8c575fa21fa
Tishana <tishana.trainor@gmail.com>
2023-08-22 14:16:21 -0400

    For the class

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header'
+import Board from './components/Board'
+import './style.css'
+export default function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Board />
+    </div>
+  )
+}
---
+++ src/components/Board.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './Card'
+import './board.css'
+
+export default function Board() {
+  return (
+    <div className='Board'>
+        <h1>The Board</h1>
+        <ul>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        </ul>
+        
+    </div>
+  )
+}
