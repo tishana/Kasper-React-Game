5b43a7178c719a0ded5eac2e9918d04549af8385
Tishana <tishana.trainor@gmail.com>
2023-08-24 11:59:46 -0400

    Day 2 mid-lesson push

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,44 @@ import Board from './components/Board'
 import './app.css'
 export default function App() {
   // setting state
-  const [newCard, setNewCard] = useState({})
-  const [playerName, setPlayerName] = useState("Sara")
+  const [card, setCard] = useState({
+    name: "Frodo",
+    race: "Hobbit",
+    skills: "Sneak, Steal",
+  });
+  // const [cardName, setCardName] = useState('N/A')
+  // const [cardRace, setCardRace] = useState('N/A')
+  // const [cardSkills, setCardSkills] = useState('N/A')
+
+  // functions
+  const handleNameChange = (e) => {
+    console.log(e.target.value)
+    setCardName(e.target.value)
+  }
+
+  const handleRaceChange = (e) => {
+    console.log(e.target.value)
+    setCardRace(e.target.value)
+  }
+
+  const handleSkillsChange = (e) => {
+    console.log(e.target.value)
+    setCardSkills(e.target.value)
+  }
   // function to create a new card
   // a button to create new cards
   return (
     <div className="App">
-      <h1>{playerName}</h1>
+      <h1>Name: {card.name}</h1>
+      <h1>Race: {card.race}</h1>
+      <h1>Skills: {card.skills}</h1>
       <div className='inputs'>
-        <input placeholder='Player Name' />
-        <input placeholder='Character Name'/>
-        <input placeholder='Character Race'/>
-        <input placeholder='Character Skills'/>
-        {/* SUBMIT BUTTON */}
-        <button>Create Card</button>
+        {/* <input placeholder='Player Name' /> */}
+        <input placeholder='Character Name' onChange={handleNameChange}/>
+        <input placeholder='Character Race' onChange={handleRaceChange}/>
+        <input placeholder='Character Skills' onChange={handleSkillsChange}/>
+        {/* SUBMIT BUTTON*/}
+        {/* <button onClick={() =>{setPlayerName('Sarah')}}>Create Card</button>  */}
       </div>
         <Board />
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,18 @@ import { useState } from 'react'
 import './card.css'
 
 export default function Card(props) {
-  const [playerName, setPlayerName] = useState('NA')
-  const [ characterName, setCharacterName] = useState('NA')
-  const [ characterRace, setCharacterRace] = useState('NA')
-  const [ passiveSkills, setPassiveSkills] = useState('NA')
-  const [ bio, setBio ] = useState('NA')
+  // const [playerName, setPlayerName] = useState('NA')
+  // const [ characterName, setCharacterName] = useState('NA')
+  // const [ characterRace, setCharacterRace] = useState('NA')
+  // const [ passiveSkills, setPassiveSkills] = useState('NA')
+  // const [ bio, setBio ] = useState('NA')
 
   return (
     <div className="Card">
         <div className='cardTop'>
-        <h2>{playerName}</h2>
-        <h4>{characterName}</h4>
-        <h4>{characterRace}</h4>
-        <h4>{passiveSkills}</h4>
+        <h2>name</h2>
+        <h4>race</h4>
+        <h4>skills</h4>
         </div>
         <div className='cardBottom'>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia repellendus deserunt inventore provident velit corporis praesentium obcaecati eos accusamus non, nam enim maxime animi libero harum at excepturi molestiae nobis.</p>
