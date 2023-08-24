80dad7a20a9d9fd1071d703a9d76a9643368e20e
Tishana <tishana.trainor@gmail.com>
2023-08-24 14:31:01 -0400

    Just about to map out the state in Board and Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,71 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './components/Header'
 import Board from './components/Board'
 import './app.css'
 export default function App() {
+
   // setting state
   const [card, setCard] = useState({
     name: "Frodo",
     race: "Hobbit",
     skills: "Sneak, Steal",
   });
+
+  const [cards, setCards] = useState([])
   // const [cardName, setCardName] = useState('N/A')
   // const [cardRace, setCardRace] = useState('N/A')
   // const [cardSkills, setCardSkills] = useState('N/A')
 
+  const cardNameRef = useRef(null)
+  const cardRaceRef = useRef(null)
+  const cardSkillsRef = useRef(null)
+  // Arrays and Objects
+
   // functions
   const handleNameChange = (e) => {
-    console.log(e.target.value)
-    setCardName(e.target.value)
+    // console.log(e.target.value)
+    // setCardName(e.target.value)
   }
 
   const handleRaceChange = (e) => {
-    console.log(e.target.value)
-    setCardRace(e.target.value)
+    // console.log(e.target.value)
+    // setCardRace(e.target.value)
   }
 
   const handleSkillsChange = (e) => {
-    console.log(e.target.value)
-    setCardSkills(e.target.value)
+    // console.log(e.target.value)
+    // setCardSkills(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    const newCard = {
+      name: cardNameRef.current.value,
+      race: cardRaceRef.current.value,
+      skills: cardSkillsRef.current.value
+    }
+    setCard(newCard)
+
+    const oldArr = cards
+    oldArr.push(newCard)
+    setCards(oldArr)
+    console.log(cards)
   }
   // function to create a new card
   // a button to create new cards
   return (
     <div className="App">
-      <h1>Name: {card.name}</h1>
+      {/* <h1>Name: {card.name}</h1>
       <h1>Race: {card.race}</h1>
-      <h1>Skills: {card.skills}</h1>
+      <h1>Skills: {card.skills}</h1> */}
       <div className='inputs'>
         {/* <input placeholder='Player Name' /> */}
-        <input placeholder='Character Name' onChange={handleNameChange}/>
-        <input placeholder='Character Race' onChange={handleRaceChange}/>
-        <input placeholder='Character Skills' onChange={handleSkillsChange}/>
+        <input placeholder='Character Name' ref={cardNameRef}/>
+        <input placeholder='Character Race' ref={cardRaceRef}/>
+        <input placeholder='Character Skills' ref={cardSkillsRef} />
         {/* SUBMIT BUTTON*/}
-        {/* <button onClick={() =>{setPlayerName('Sarah')}}>Create Card</button>  */}
+        <button onClick={handleSubmit}>Create Card</button> 
       </div>
-        <Board />
+        <Board cards={cards}/>
     </div>
   )
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import Card from './Card'
 import './board.css'
 
-export default function Board() {
+export default function Board(props) {
   return (
     <div className='Board'>
         <h1>The Board</h1>
         <ul>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-    
+          {props.cards.map(card =>{
+            return <Card card={card}/>
+          })}
         </ul>
         
     </div>
